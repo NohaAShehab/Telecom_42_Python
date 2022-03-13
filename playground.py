68d8c2d4791ab4be9b3f9cf772c19560981185b6
Noha Shehab <nohashehab.iti@gmail.com>
2022-03-13 13:07:59 +0200

    day01

---
+++ playground.py
@@ -0,0 +1,7 @@
+# txt = "Iti is a very well known institute"  #
+# # print(txt.capitalize())
+# print(txt.islower())
+# print(txt.isupper())
+
+
+input("plz enter your name: ")
---
+++ test.py
@@ -0,0 +1,35 @@
+print("First day in python")
+
+# print("First day in python")
+# # parser
+# print("First day in python")
+#
+# print("First day in python")
+'''
+python is loosly dynamically typed lang..
+loosly --> the interpreter detect the value of the variable in
+the run time 
+dynamically --> you can change datatype of variable in the run 
+time
+
+'''
+x = 10
+print(x)
+'''to check the data type of variable '''
+print(type(x))  # ## object from class type
+
+
+# mycv = """
+# this is Ayman,
+# studies at telecom Track
+# Information technology Institue
+#
+# """
+
+mycv = '''
+this is Ayman, 
+studies at telecom Track
+Information technology Institue
+'''
+
+print(mycv)
